25eecf3e9d212941c5e14a7c3b38191c104343a2
Carlos H Mora Jr <carlosmorajunior@gmail.com>
2020-01-22 17:08:05 -0300

    Initial commit

---
+++ band/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Band(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    origin = models.CharField(max_length=200)
+    image = models.CharField(max_length=200)
+    bio = models.TextField(max_length=2000)
